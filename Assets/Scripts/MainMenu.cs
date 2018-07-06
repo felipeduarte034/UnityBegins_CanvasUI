d78c6f6ae077774bf81a9aa12928edb0276f1bad
Felipe Duarte <felipeduarte034@gmail.com>
2018-07-05 23:47:05 -0300

    first commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private int cont = 0;
+
+    public void CliqueiNoButao()
+    {
+        print(cont++);
+    }
+
+    public void CliqueZero()
+    {
+        cont = 0;
+		print(cont);
+    }
+
+	public void OnClickPlay()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	public void OnClickSair()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}
